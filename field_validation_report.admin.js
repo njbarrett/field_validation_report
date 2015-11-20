e7fd8e888d886c2b7ea9c5e4565f4740c646b7ed
Nick Barrett <njbarrett7@gmail.com>
2015-11-20 06:02:51 +0000

    further improve the UX of the report

--- field_validation_report.admin.js
+++ field_validation_report.admin.js
@@ -3,11 +3,21 @@
   Drupal.behaviors.fieldValidationReport = {
     attach: function (context, settings) {
       $('.field-validation-report__entity-type-label').bind('click', function(e) {
+        var $entityTypeLabel = $(this);
         var entityType = $(this).closest('.field-validation-report__entity-type').data('entity-type');
-        $('.field-validation-report__bundle.field-validation-report__entity-type--' + entityType)
-          .toggle()
-          .toggleClass('expanded');
+        var $bundles = $('.field-validation-report__bundle.field-validation-report__entity-type--' + entityType);
+        $bundles.toggle().toggleClass('expanded');
         $(this).toggleClass('expanded');
+        $bundles.each(function(i, bundle) {
+          if ($entityTypeLabel.hasClass('expanded')) {
+            $('.field-validation-report__bundle-label.field-validation-report__bundle--' + $(bundle).data('bundle')).addClass('expanded');
+            $('.field-validation-report__entity.field-validation-report__bundle--' + $(bundle).data('bundle')).show();
+          }
+          else {
+            $('.field-validation-report__bundle-label.field-validation-report__bundle--' + $(bundle).data('bundle')).removeClass('expanded');
+            $('.field-validation-report__entity.field-validation-report__bundle--' + $(bundle).data('bundle')).hide();
+          }
+        });
       });
       $('.field-validation-report__bundle-label').bind('click', function(e) {
         var bundle = $(this).closest('.field-validation-report__bundle').data('bundle');
